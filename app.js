000f6d2b19b0e94004db8ab1fb80bc1ccf491e88
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-17 21:14:37 +0530

    added post addition sync

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ mongoose
   .connect(process.env.MONGO)
   .then(result => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server);
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected');
     });
