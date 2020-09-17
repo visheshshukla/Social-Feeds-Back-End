b518e4625baa179c0c93a674c7bb833ae34e818c
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-17 20:36:33 +0530

    added client connection

--- app.js
+++ app.js
@@ -62,6 +62,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGO)
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
